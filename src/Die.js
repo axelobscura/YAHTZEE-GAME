d1cea53e0659ad637250bcafc5bc2c242528ea95
Axel <axosar@gmail.com>
2019-07-29 18:59:00 -0500

    Dice handlers
    Finishing dice handlers

--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,7 @@ import "./Die.css";
 class Die extends Component {
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bond(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick(){
     this.props.handleClick(this.props.idx)
